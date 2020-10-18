c88b73d8c24f96772abfedc6bf49a910248e17ff
Dwtexe <63106683+Dwtexe@users.noreply.github.com>
2020-10-18 16:46:38 +0300

    operators

---
+++ 3-Operators/type_test_op.dart
@@ -0,0 +1,10 @@
+main() {
+//  Type Test Operatörleri: is,is!,as(Python kütüphane ad değişimi gibi)
+
+  int tamsayi = 10;
+
+  print(tamsayi is int);
+  print(100 is int);
+
+  print(tamsayi is! String);
+}
